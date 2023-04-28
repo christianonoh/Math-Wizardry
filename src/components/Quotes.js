951b39ac75e995a71679b21d2b1accefda2ce72b
christianonoh <onohchibyk@gmail.com>
2023-04-28 03:31:13 +0100

    Modify Quotes to catch errors

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -5,32 +5,34 @@ function Quotes({ category }) {
   const [quoteObj, setQuoteObj] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [requestOptions] = useState({
+  const apiUrl = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+  const requestOptions = {
     method: 'GET',
-    headers: { 'X-Api-Key': 'm98830p1s59J/QGTjBLHqQ==Fj1asIwzPVGdbNce' },
+    headers: { 'X-Api-Key': 'sSdFja7TYGhiP1vBvnhoNwolcjjCf3jircreEzZn' },
     contentType: 'application/json',
-  });
-  const [apiUrl] = useState(`https://api.api-ninjas.com/v1/quotes?category=${category}`);
-
-  Quotes.propTypes = { category: PropTypes.string.isRequired };
+  };
 
   const fetchData = async () => {
     try {
+      setLoading(true);
+      setError(null);
       const res = await fetch(apiUrl, requestOptions);
+      if (!res.ok) {
+        throw new Error('Error fetching quote.');
+      }
       const json = await res.json();
-      setQuoteObj(json);
       setLoading(false);
+      setQuoteObj(json);
       console.log('Sent request');
     } catch (error) {
-      setError(true);
+      setError('Error fetching quote.');
+      setQuoteObj([]);
       setLoading(false);
       console.log('Sent request but failed');
     }
   };
 
   useEffect(() => {
-    setLoading(true);
-    setError(null);
     fetchData();
   }, []);
 
@@ -39,26 +41,30 @@ function Quotes({ category }) {
   }
 
   if (error) {
-    return <div className="quote-container">Error fetching quote.</div>;
+    return <div className="quote-container">{error}</div>;
   }
 
   return (
     <div className="quote-container">
       <h1>
-        {category.charAt(0).toUpperCase() + category.slice(1)}
+        {category?.charAt(0)?.toUpperCase() + category?.slice(1)}
         {' '}
         Quotes
       </h1>
       {Array.isArray(quoteObj) && quoteObj.map((element) => (
-        <div key={element.author}>
+        <div key={element?.author}>
           <blockquote className="quote">
-            {element.quote}
+            {element?.quote}
           </blockquote>
-          <h5><i>{element.author}</i></h5>
+          <h5><i>{element?.author}</i></h5>
         </div>
       ))}
     </div>
   );
 }
+
+Quotes.propTypes = {
+  category: PropTypes.string.isRequired,
+};
 
 export default Quotes;
